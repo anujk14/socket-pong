32445955c8ddda2b98f69ea059a5cac9ce22522b
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2019-07-11 23:51:21 +0530

    Adds socket server to main HTTP server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var WebSocketServer = require('./src/WebSocketServer');
 
 var HTTPServer = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write("Hello");
+
+  clientInfo = JSON.stringify({ "ids": wss.getClients() })
+
+  res.write(clientInfo);
   res.end();
 }).listen(8000, function() {
   console.log("Running on port 8000........");
 });
+
+var wss = new WebSocketServer(HTTPServer);
